453452925b23390330014fd2a6e1439b2321abd4
bookert921 <bookert921@gmail.com>
2020-12-16 15:58:25 -0800

    ties in email.js for contact form
    contact page only work when user is logged in.

--- pages/about.js
+++ pages/about.js
@@ -8,20 +8,19 @@ function About() {
 
   return (
     <Layout user={user} loading={loading}>
-            <h1 className={styles.title}>
-              BOOK and BEYOND
-            </h1>
-            <h3>About</h3>
-          <p>
-            Untimately, this is an app decidated to growth, community, and learning potentials realized. My hope is that it grows into an all inclusive study space with management tools for prioritizing and optimizing study sessions.
-          </p>
-          <p>
-            Learning can be hard enough as it is. There is so much to know and so little time to do it all. In addition, all the tools and resources you may use to manage your sessions are all over the place. Let us manage help you manage your productivity all in one place. 
-          </p>    
-          <p>
-            The product roadmap is to develop a Communication, Project Management,  and Knowledge Management integrated suite of applications all dedicated to getting the most out of your study time.  
-          </p>  
-        
+      <h1 className={styles.title}>
+        BOOK and BEYOND
+      </h1>
+      <h3>About</h3>
+      <p>
+        Untimately, this is an app decidated to growth, community, and learning potentials realized. My hope is that it grows into an all inclusive study space with management tools for prioritizing and optimizing study sessions.
+      </p>
+      <p>
+        Learning can be hard enough as it is. There is so much to know and so little time to do it all. In addition, all the tools and resources you may use to manage your sessions are all over the place. Let us manage help you manage your productivity all in one place. 
+      </p>    
+      <p>
+        The product roadmap is to develop a Communication, Project Management,  and Knowledge Management integrated suite of applications all dedicated to getting the most out of your study time.  
+      </p>  
     </Layout>
   )
 }
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,62 @@
+import {useState} from 'react'
 import Layout from '../components/layout'
+import emailjs, {init} from 'emailjs-com'
+import { useFetchUser } from '../lib/user'
+import { set } from 'date-fns'
+
 export default function Contact() {
+    const { user } = useFetchUser()
+    // init("user_ZaGeVt3pdHwsfPvskHNAf")
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    
+    function sendEmail(e) {
+        e.preventDefault();
+        
+        emailjs.sendForm('service_ja7ytez', 'template_u5l5gj9', e.target, 'user_ZaGeVt3pdHwsfPvskHNAf')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        
+        setName('')
+        setEmail('')
+        setMessage('')
+    }
+
     return (
-        <Layout>
-            <div>
-                Hi
-            </div>
+        <Layout user={user} >
+            <form className="contact-form" onSubmit={sendEmail}>
+                <label htmlFor="name">Name</label>
+                <input
+                    placeholder=""
+                    value={user.name} 
+                    type="text" 
+                    className="form-control" 
+                    name={user.name}  
+                    onChange={e => 
+                    setName(e.target.value)}    
+                />
+                <label htmlFor="email">Email</label>
+                <input
+                    placeholder={user.email}
+                    value={user.email} 
+                    type="email" 
+                    className="form-control" 
+                    name="user_email"  
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <label>Message</label>
+                <textarea
+                    name="message" 
+                    value={message} 
+                    onChange={e => setMessage(e.target.value)}
+                    className="form-control"
+                />
+                <button type="submit" value="send" className="btn btn-primary">Submit</button>
+            </form>
         </Layout>
     )
 }
